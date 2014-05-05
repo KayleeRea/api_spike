b339bef2c5a0727a94f18a401ff8564a9b3d1f3f
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-05-05 11:38:17 -0600

    Gives the current decription and temperature for Key West, FL

---
+++ key_west_weather.rb
@@ -0,0 +1,13 @@
+require 'faraday'
+require 'json'
+
+
+response = Faraday.get "http://api.openweathermap.org/data/2.5/weather?q=Key%20West,us"
+json_data = response.body
+
+description = JSON.parse(json_data)["weather"].map { |description| description["description"]}
+temperature = JSON.parse(json_data)["main"]["temp"]
+
+#gives the current description and temperature for Key West, FL
+puts "The current weather for Key West is:"
+puts description, temperature
